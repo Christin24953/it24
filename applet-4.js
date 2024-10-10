9a5c9079c3a1bb05158561b8dc1a7fbc62317ecf
yanyan <christianreyboconsanico@gmail.com>
2024-10-10 22:59:16 +0800

    Added filterStudents

--- applet-4.js
+++ applet-4.js
@@ -41,3 +41,10 @@ class StudentList {
 
         this.renderStudentList(this.students, studentSearchListContainer);
     }
+
+    filterStudents(query, searchListContainer) {
+        const filteredStudents = this.students.filter(student => {
+            const fullName = `${student.student_name} ${student.student_program}`;
+            return fullName.toLowerCase().includes(query.toLowerCase());
+        });
+
