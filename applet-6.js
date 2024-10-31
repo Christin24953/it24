5ca80d693c64b5f987c76920692d72b845a4df35
christin24953 <christianreyboconsanico@gmail.com>
2024-10-31 18:51:27 +0800

    added resetEditing

--- applet-6.js
+++ applet-6.js
@@ -55,6 +55,13 @@ class todoList{
         this.editingIndex = Array.from(this.todoList.children).indexOf(taskItem);
         this.addButton.textContent = 'Update';
     }
+    resetEditing() {
+        this.editingIndex = -1;
+        this.addButton.textContent = 'Add';
+    }
+
+}
+
 
 
 
